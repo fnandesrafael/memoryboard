e797e201d7c45d40473d48217691508f550c4a91
fnandesrafael <fnandesrafael@pm.me>
2023-09-05 15:51:34 -0300

    feat: basic textures and backgrounds

--- src/global.styles.ts
+++ src/global.styles.ts
@@ -1,4 +1,6 @@
 import styled, { createGlobalStyle } from 'styled-components';
+import grunge from '@assets/textures/grunge.png';
+import grid from '@assets/textures/grid.png';
 
 export const Overall = createGlobalStyle`
   * {
@@ -13,10 +15,40 @@ export const Overall = createGlobalStyle`
   body {
     background-color: #131313;
     color: #B3B0AA;
+
+    &::after {
+      background-image: ${`url(${grunge})`};
+      background-size: cover;
+      content: '';
+      height: 100%;
+      left: 0;
+      mix-blend-mode: luminosity;
+      opacity: 0.5;
+      pointer-events: none;
+      position: fixed;
+      top: 0;
+      width: 100%;
+      z-index: 10;
+    }
   }
 `;
 
 export const AppWrapper = styled.div`
   height: 100%;
   width: 100%;
+
+  &::after {
+    background-image: ${`url(${grid})`};
+    background-size: cover;
+    content: '';
+    height: 100%;
+    left: 0;
+    mix-blend-mode: luminosity;
+    opacity: 0.75;
+    pointer-events: none;
+    position: fixed;
+    top: 0;
+    width: 100%;
+    z-index: 10;
+  }
 `;
