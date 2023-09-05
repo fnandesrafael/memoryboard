a83efae6cce35b56f5a728e8f1907681b4f881e8
fnandesrafael <fnandesrafael@pm.me>
2023-09-05 16:33:45 -0300

    feat: Importer component first implementation

---
+++ src/components/Importer/Importer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { GrFormAdd } from 'react-icons/gr';
+
+import * as S from './styles';
+
+export default function Importer() {
+  return (
+    <S.Button>
+      <GrFormAdd />
+    </S.Button>
+  );
+}
---
+++ src/components/Importer/styles.ts
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+import { motion } from 'framer-motion';
+
+export const Button = styled(motion.button)`
+  background-color: #0a0a0a;
+  border: none;
+  border-radius: 100%;
+  bottom: 2rem;
+  height: 4rem;
+  opacity: 0.5;
+  outline: none;
+  padding: 1rem;
+  position: fixed;
+  right: 2rem;
+  transition: 0.2s opacity;
+  width: 4rem;
+  z-index: 10;
+
+  &:hover {
+    cursor: pointer;
+    opacity: 1;
+    transition: 0.2s opacity;
+  }
+
+  svg,
+  path {
+    font-size: 2rem;
+    stroke: #b0b3aa;
+  }
+`;
+
+export default Button;
