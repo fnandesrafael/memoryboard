8c3c2ad9e24b8e40514fe086f9cc257a35b7783f
fnandesrafael <fnandesrafael@pm.me>
2023-09-06 11:24:25 -0300

    fix: MIME type in Dropzone.tsx file

--- src/components/Dropzone/Dropzone.tsx
+++ src/components/Dropzone/Dropzone.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useDropzone } from 'react-dropzone';
 
 import * as S from './styles';
 
 export default function Dropzone() {
-  const { getRootProps, getInputProps } = useDropzone({
-    accept: { 'images/*': [] },
+  const { acceptedFiles, getRootProps, getInputProps } = useDropzone({
+    accept: { 'image/*': [] },
   });
+
+  useEffect(() => {
+    console.log(acceptedFiles);
+  }, [acceptedFiles]);
 
   return (
     <S.Container {...getRootProps()}>
