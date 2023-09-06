93ac8e78921497578a185817f0c257bfeabb48f1
fnandesrafael <fnandesrafael@pm.me>
2023-09-06 12:30:45 -0300

    feat: Dropzone styling by drag activation

--- src/components/Dropzone/styles.ts
+++ src/components/Dropzone/styles.ts
@@ -2,10 +2,46 @@ import styled from 'styled-components';
 import { motion } from 'framer-motion';
 import { DropzoneRootProps } from 'react-dropzone';
 
-export const Container = styled(motion.div)<DropzoneRootProps>`
-  border: 1px solid red;
+type DropzoneContainer = {
+  $isDragActive: boolean;
+} & DropzoneRootProps;
+
+export const Container = styled(motion.div)<DropzoneContainer>`
+  align-items: center;
+  background-color: ${({ $isDragActive }) =>
+    $isDragActive ? '#1a1a1a' : '#0a0a0a'};
+  border-radius: 0.5rem;
+  display: flex;
+  flex-flow: column;
   height: 100%;
+  justify-content: center;
+  transition: 0.2s background-color;
   width: 100%;
+
+  svg {
+    font-size: 2.5rem;
+  }
+
+  &:hover {
+    background-color: #1a1a1a;
+    cursor: pointer;
+    transition: 0.2s background-color;
+  }
+`;
+
+export const Heading = styled(motion.h1)`
+  color: #b3b0aa;
+  font-family: Nunito, Ubuntu, sans-serif;
+  font-size: 1.25rem;
+  font-weight: 600;
+  margin-top: 1rem;
+`;
+
+export const Paragraph = styled(motion.p)`
+  color: #b3b0aa;
+  font-family: Nunito, Ubuntu, sans-serif;
+  font-size: 1rem;
+  font-weight: 200;
 `;
 
 export default Container;
--- src/components/Modal/styles.ts
+++ src/components/Modal/styles.ts
@@ -26,6 +26,9 @@ export const Container = styled(motion.div)`
   align-items: center;
   background-color: #0a0a0a;
   border-radius: 0.5rem;
+  box-shadow:
+    0 4px 6px -1px rgb(0 0 0 / 10%),
+    0 2px 4px -2px rgb(0 0 0 / 10%);
   display: flex;
   flex-flow: column;
   height: 16rem;
