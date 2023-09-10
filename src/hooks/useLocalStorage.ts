84504ca2ccca7973e6bce61493edf619e43090ad
fnandesrafael <fnandesrafael@pm.me>
2023-09-10 15:28:08 -0300

    delete: comments

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -15,41 +15,6 @@ const useLocalStorage = () => {
   }, [setPolaroids]);
 
   return { saveInLocal };
-
-  // useEffect(() => {
-  //   const convertFiles = async () => {
-  //     const result = polaroids.map(async (polaroid) => {
-  //       const dataUrl = await new Promise((resolve) => {
-  //         const reader = new FileReader();
-
-  //         reader.onload = (event) => {
-  //           resolve(event.target.result);
-  //         };
-
-  //         reader.readAsDataURL(polaroid.file as Blob);
-  //       });
-
-  //       return { ...polaroid, file: dataUrl };
-  //     });
-
-  //     const convertedData = await Promise.all(result);
-
-  //     localStorage.setItem('polaroids', JSON.stringify(convertedData));
-  //   };
-
-  //   convertFiles();
-  // }, [polaroids]);
-
-  // useEffect(() => {
-  //   const data = JSON.parse(localStorage.getItem('polaroids')) || [];
-  //   const recoveredData = data.map((obj: PolaroidObject) => {
-  //     const blob = dataURLToBlob(obj.file as string);
-  //     const recoveredFile = new File([blob], obj.fileName);
-  //     return { ...obj, file: recoveredFile };
-  //   });
-
-  //   setPolaroids(recoveredData);
-  // }, [setPolaroids]);
 };
 
 export default useLocalStorage;
